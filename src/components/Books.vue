3afb9dc4d3224ba3d3374c888a9022f7e210b64c
AnaClaraAmorim <anaclaramorimandrade@gmail.com>
2022-06-09 17:27:07 -0300

    first commit

---
+++ src/components/Books.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home-book">
+    <img class="img-livro" src="./img/harry.jpg" width="200vw" />
+    <div class="home-book-info">
+      <p class="title">HARRY POTTER E A PEDRA FILOSOFAL</p>
+      <ul class="category">
+        <li>Aventura</li>
+        <li>Juvenil</li>
+      </ul>
+      <p class="price">R$ 30,90</p>
+      <div class="promo">Promoção!!</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home-book:hover{
+  border-radius: 0.5em;
+  box-shadow: 2px 2px 2px 2px lightgray;
+  cursor: pointer;
+}
+
+.home-book{
+    display: flex;
+    max-width: 29vw !important;
+    min-width: 28vw;
+    margin: 2vw 0;
+    margin-right: 2vw ;
+}
+
+.home-book-info{
+  padding-left: 2em  ;
+  font-family: 'Grape Nuts', cursive;
+  font-family: 'Open Sans', sans-serif;
+}
+
+.category{
+    color: #0B859F;
+}
+
+.title{
+    font-size: 1.5em;
+}
+
+.price{
+  font-size: 1.5em;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+import Books from '@/components/Books.vue'
+</script>
+
+<template>
+
+
+    <div  class="books">
+      <Books v-for="analito in checkedHemograma"/>
+    </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    
+      checkedHemograma: ['Harry', 'Crepusculo'],
+      
+      
+     
+    }
+  }
+}
+</script>
+
+<style>
+@import '@/assets/base.css';
+
+.books{
+ width: 93vw;
+    
+    padding: 0em 2vw;
+    display: flex;
+    justify-content: row;
+    flex-wrap: wrap;
+}
+
+
+</style>
