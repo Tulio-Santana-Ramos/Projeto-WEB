2bdc10d548404766cc3c7db0d52e260b22fe62b8
AnaClaraAmorim <anaclaramorimandrade@gmail.com>
2022-06-13 18:28:32 -0300

    status do promo

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -2,65 +2,57 @@
   <div class="home-book">
     <img class="img-livro" src="./img/harry.jpg" width="200vw" />
     <div class="home-book-info">
-      <p class="title">{{name}}</p>
-      <ul class="category">
-        <li>{{categorias}}</li>
-        <li>Juvenil</li>
+      <p class="title">{{ name }}</p>
+      <ul v-for="category in categories" class="category">
+        <li>{{ category }}</li>
       </ul>
-      <p class="price">R$ {{price}}</p>
-      <div class="promo">Promoção!!</div>
+      <p class="price">R$ {{ price }}</p>
+
+      <div v-if="Isinpromo" class="promo">Promoção!!</div>
     </div>
   </div>
 </template>
 
-
 <script>
 export default {
-  name: 'books',
-  props: ['name','price','categorias'],
-  data () {
-    return {
-      
-    }
-  }
-}
+  name: "books",
+  props: ["name", "price", "categories","Isinpromo"],
+  data() {
+    return {};
+  },
+};
 </script>
 
-
 <style scoped>
-
-
-
-
-.home-book:hover{
+.home-book:hover {
   border-radius: 0.5em;
   box-shadow: 2px 2px 2px 2px lightgray;
   cursor: pointer;
 }
 
-.home-book{
-    display: flex;
-    max-width: 29vw !important;
-    min-width: 28vw;
-    margin: 2vw 0;
-    margin-right: 2vw ;
+.home-book {
+  display: flex;
+  max-width: 29vw !important;
+  min-width: 28vw;
+  margin: 2vw 0;
+  margin-right: 2vw;
 }
 
-.home-book-info{
-  padding-left: 2em  ;
-  font-family: 'Grape Nuts', cursive;
-  font-family: 'Open Sans', sans-serif;
+.home-book-info {
+  padding-left: 2em;
+  font-family: "Grape Nuts", cursive;
+  font-family: "Open Sans", sans-serif;
 }
 
-.category{
-    color: #0B859F;
+.category {
+  color: #0b859f;
 }
 
-.title{
-    font-size: 1.5em;
+.title {
+  font-size: 1.5em;
 }
 
-.price{
+.price {
   font-size: 1.5em;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,90 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-import Books from '@/components/Books.vue'
+import TheWelcome from "@/components/TheWelcome.vue";
+import Books from "@/components/Books.vue";
 import Footer from "../components/Footer.vue";
-
 </script>
 
 <template>
-
-    <div class="books">
-        <div v-for="book in books">
-          <Books  :name="book.name" :price="book.price" :categoria = "book.categorias"/>
-        </div>
+  <div class="books">
+    <div v-for="book in books">
+      <Books
+        :name="book.name"
+        :price="book.price"
+        :categories="book.categories"
+        :Isinpromo = "book.promo"
+      />
     </div>
-  <Footer>
-
-  </Footer>
+  </div>
+  <Footer> </Footer>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
       books: [
-        {name:'Harry Potter e a pedra filosofal',categorias:["Aventura","Juvenil"],price: "30.90"}, 
-        {name:'Crepusculo',categorias:["Aventura","Juvenil"],price: "30.90"}
-        ],   
-    }
-  }
-}
+        {
+          name: "Harry Potter e a pedra filosofal",
+          categories: ["Aventura", "Juvenil"],
+          price: "30.90",
+          promo: true
+        },
+        {
+          name: "Crepusculo",
+          categories: ["Aventura", "Juvenil"],
+          price: "30.90",
+          promo: true
+        },
+        {
+          name: "Harry Potter e a pedra filosofal",
+          categories: ["Aventura", "Juvenil"],
+          price: "30.90",
+          promo: false
+        },
+        {
+          name: "Crepusculo",
+          categories: ["Aventura", "Juvenil"],
+          price: "30.90",
+          promo: false
+        },
+        {
+          name: "Harry Potter e a pedra filosofal",
+          categories: ["Aventura", "Juvenil"],
+          price: "30.90",
+          promo: false
+        },
+        {
+          name: "Crepusculo",
+          categories: ["Aventura", "Juvenil"],
+          price: "30.90",
+          promo: false
+        },
+        {
+          name: "Harry Potter e a pedra filosofal",
+          categories: ["Aventura", "Juvenil"],
+          price: "30.90",
+          promo: false
+        },
+        {
+          name: "Crepusculo",
+          categories: ["Aventura", "Juvenil"],
+          price: "30.90",
+          promo: false
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-@import '@/assets/base.css';
+@import "@/assets/base.css";
 
-.books{
- width: 93vw;
-    
-    padding: 0em 2vw;
-    display: flex;
-    justify-content: row;
-    flex-wrap: wrap;
+.books {
+  padding: 0em 2vw;
+  display: flex;
+  justify-content: row;
+  flex-wrap: wrap;
 }
-
-
 </style>
