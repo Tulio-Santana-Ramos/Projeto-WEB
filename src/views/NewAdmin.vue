cc1fe6cfb785dc63cf29e205f61498ee8e8d8805
Tulip SR <tulip@White-Room.localdomain>
2022-06-14 05:21:51 -0300

    Add New Account Pages

---
+++ src/views/NewAdmin.vue
@@ -0,0 +1,26 @@
+<script setup>
+import NewAccount from "@/components/NewAccount.vue";
+</script>
+
+<template>
+    <img class="admin-pic" src="@/components/icons/admin-with-cogwheels.png"/>
+    <h2 class="title">Adicionar admin</h2>
+    <NewAccount/>
+</template>
+
+<style scoped>
+
+.title{
+    font-weight: normal;
+    text-align: center;
+    margin-top: 90px;
+    margin-left: 175px;
+}
+
+.admin-pic{
+    float: right;
+    margin-top: 100px;
+    margin-right: 50px;
+    height: 120px;
+}
+</style>
---
+++ src/views/NewUser.vue
@@ -0,0 +1,41 @@
+<script setup>
+import NewAccount from "@/components/NewAccount.vue";
+</script>
+
+<template>
+    <h2 class="title">CRIAR CONTA</h2>
+    <NewAccount/>
+    <p class="use-conditions">Condições de uso</p>
+    <p class="contact">Entre em contato</p>
+</template>
+
+<style scoped>
+
+.title{
+    font-weight: normal;
+    text-align: center;
+    margin-top: 90px;
+}
+.use-conditions{
+    color: #38B6FF;
+    margin-left: 400px;
+    margin-top: 75px;
+    font-size: 1.5em;
+    display: inline-block;
+}
+
+.contact{
+    color: #38B6FF;
+    margin-left: 760px;
+    margin-top: 75px;
+    font-size: 1.5em;
+    display: inline-block;
+}
+.use-conditions:hover{
+    color: black;
+}
+
+.contact:hover{
+    color: black;
+}
+</style>
