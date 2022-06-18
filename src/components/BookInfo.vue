cbbeef3f1f280ff276df3f345c1c0537e26b17d6
EduardoPortoSilva <edu.porto51@gmail.com>
2022-06-17 22:33:02 -0300

    Programmatically add categories

--- src/components/BookInfo.vue
+++ src/components/BookInfo.vue
@@ -3,6 +3,95 @@
 <template>
 
   <div class="book-container">
+    <!-- The Modal -->
+    <div id="modalChangeInfos" class="modal" style="margin-top: 200px;overflow: auto">
+      <div class="modal-dialog">
+        <div class="modal-content">
+
+          <!-- Modal Header -->
+          <div class="modal-header">
+            <h4 class="modal-title">Alterar informações do livro</h4>
+            <button class="btn-close" data-bs-dismiss="modal" type="button"></button>
+          </div>
+          <div style="overflow-y: scroll; height: 600px; padding-left: 10px; padding-right: 10px">
+            <div class="input-group input-container">
+            <span class="input-group-text" style="width: 87px;">
+              Nome
+            </span>
+              <input class="form-control" placeholder="Nome do livro"
+                     style="width: 350px;height: 45px;font-size: medium"
+                     ref="input_nome" v-bind:value="name">
+            </div>
+            <br>
+            <div class="input-group input-container" style="margin-right: auto; margin-left: auto;">
+            <span class="input-group-text" style="width: 87px">
+              Preço
+            </span>
+              <input class="form-control" placeholder="Preço do livro" style="height: 45px;font-size: medium"
+                     type="text" ref="input_preco" v-bind:value="price">
+            </div>
+            <br>
+            <div class="input-group input-container" style="margin-right: auto; margin-left: auto;">
+            <span class="input-group-text" style="width: 87px">
+              Sinopse
+            </span>
+              <textarea class="form-control" placeholder="Sinopse do livro" style="resize: none;height: 100px;font-size: medium"
+                        type="text" ref="input_sinopse" v-bind:value="synopsis"/>
+            </div>
+            <br>
+            <div class="input-group input-container" style="margin-right: auto; margin-left: auto;">
+            <span class="input-group-text" style="width: 87px">
+              Editora
+            </span>
+              <input class="form-control" placeholder="Nome da editora" style="height: 45px;font-size: medium"
+                     type="text" ref="input_editora" v-bind:value="editor">
+            </div>
+            <br>
+            <div class="input-group input-container" style="margin-right: auto; margin-left: auto;">
+            <span class="input-group-text" style="width: 87px">
+              Autor
+            </span>
+              <input class="form-control" placeholder="Nome do autor" style="height: 45px;font-size: medium"
+                     type="text" ref="input_autor" v-bind:value="author">
+            </div>
+            <br>
+            <div class="input-group input-container" style="margin-right: auto; margin-left: auto;">
+            <span class="input-group-text" style="width: 87px">
+              Tradutor
+            </span>
+              <input class="form-control" placeholder="Nome do tradutor" style="height: 45px;font-size: medium"
+                     type="text" ref="input_tradutor" v-bind:value="tradutor">
+            </div>
+            <br>
+            <div class="input-group input-container" style="margin-right: auto; margin-left: auto;">
+            <span class="input-group-text" style="width: 87px">
+              Ano
+            </span>
+              <input class="form-control" placeholder="Ano de publicação" style="height: 45px;font-size: medium"
+                     type="text" ref="input_ano" v-bind:value="year">
+            </div>
+            <div v-for="(category,index) in getAllCategories()" :key="updateCategories">
+              <input v-model="categoriesCheck[index]" type="checkbox">
+              <label>{{
+                  category.name
+                }}</label>
+
+            </div>
+            <div>
+              Adicionar nova categoria
+              <input class="form-control" placeholder="Nova categoria" style="height: 45px;font-size: medium"
+                     type="text" ref="newCategory" >
+              <button class="btn btn-primary" type="button" @click="addCategory(this.$refs.newCategory.value)">Adicionar categoria</button>
+            </div>
+          </div>
+          <!-- Modal footer -->
+          <div class="modal-footer">
+            <button class="btn btn-primary" data-bs-dismiss="modal" @click="updateLivro(this.$refs.input_nome,this.$refs.input_preco,this.$refs.input_sinopse,this.$refs.input_editora,this.$refs.input_autor,this.$refs.input_tradutor,this.$refs.input_ano)" type="button">Alterar</button>
+            <button class="btn btn-danger" data-bs-dismiss="modal" type="button">Cancelar</button>
+          </div>
+        </div>
+      </div>
+    </div>
     <div v-if="promotion.is" class="promo">
       Quantidade promocional restante: {{ promotion.numberBooks }}
     </div>
@@ -50,92 +139,6 @@
   </div>
 
   <h2 class="title-evaluation">Avaliações</h2>
-
-  <!-- The Modal -->
-  <div id="modalChangeInfos" class="modal" style="margin-top: 200px">
-    <div class="modal-dialog">
-      <div class="modal-content">
-
-        <!-- Modal Header -->
-        <div class="modal-header">
-          <h4 class="modal-title">Alterar informações do livro</h4>
-          <button class="btn-close" data-bs-dismiss="modal" type="button"></button>
-        </div>
-        <div class="input-group input-container">
-          <span class="input-group-text" style="width: 87px;">
-            Nome
-          </span>
-          <input class="form-control" placeholder="Nome do livro"
-                 style="width: 350px;height: 45px;font-size: medium"
-                 ref="input_nome" v-bind:value="name">
-        </div>
-        <br>
-        <div class="input-group input-container" style="margin-right: auto; margin-left: auto;">
-          <span class="input-group-text" style="width: 87px">
-            Preço
-          </span>
-          <input class="form-control" placeholder="Preço do livro" style="height: 45px;font-size: medium"
-                 type="text" ref="input_preco" v-bind:value="price">
-        </div>
-        <br>
-        <div class="input-group input-container" style="margin-right: auto; margin-left: auto;">
-          <span class="input-group-text" style="width: 87px">
-            Sinopse
-          </span>
-          <textarea class="form-control" placeholder="Sinopse do livro" style="resize: none;height: 100px;font-size: medium"
-                 type="text" ref="input_sinopse" v-bind:value="synopsis"/>
-        </div>
-        <br>
-        <div class="input-group input-container" style="margin-right: auto; margin-left: auto;">
-          <span class="input-group-text" style="width: 87px">
-            Editora
-          </span>
-          <input class="form-control" placeholder="Nome da editora" style="height: 45px;font-size: medium"
-                 type="text" ref="input_editora" v-bind:value="editor">
-        </div>
-        <br>
-        <div class="input-group input-container" style="margin-right: auto; margin-left: auto;">
-          <span class="input-group-text" style="width: 87px">
-            Autor
-          </span>
-          <input class="form-control" placeholder="Nome do autor" style="height: 45px;font-size: medium"
-                 type="text" ref="input_autor" v-bind:value="author">
-        </div>
-        <br>
-        <div class="input-group input-container" style="margin-right: auto; margin-left: auto;">
-          <span class="input-group-text" style="width: 87px">
-            Tradutor
-          </span>
-          <input class="form-control" placeholder="Nome do tradutor" style="height: 45px;font-size: medium"
-                 type="text" ref="input_tradutor" v-bind:value="tradutor">
-        </div>
-        <br>
-        <div class="input-group input-container" style="margin-right: auto; margin-left: auto;">
-          <span class="input-group-text" style="width: 87px">
-            Ano
-          </span>
-          <input class="form-control" placeholder="Ano de publicação" style="height: 45px;font-size: medium"
-                 type="text" ref="input_ano" v-bind:value="year">
-        </div>
-        <div v-for="(category,index) in getAllCategories()">
-          <!--<input v-if="searchInActualCategories(category.id)" v-model="categoriesCheck" type="checkbox" v-bind:value="category.id" checked>
-          <input v-else v-model="categoriesCheck" type="checkbox" v-bind:value="category.id">-->
-          <input v-model="categoriesCheck[index]" type="checkbox">
-          <label>{{
-            category.name
-          }}</label>
-
-        </div>
-
-        <!-- Modal footer -->
-        <div class="modal-footer">
-          <button class="btn btn-primary" data-bs-dismiss="modal" @click="updateLivro(this.$refs.input_nome,this.$refs.input_preco,this.$refs.input_sinopse,this.$refs.input_editora,this.$refs.input_autor,this.$refs.input_tradutor,this.$refs.input_ano)" type="button">Alterar</button>
-          <button class="btn btn-danger" data-bs-dismiss="modal" type="button">Cancelar</button>
-        </div>
-
-      </div>
-    </div>
-  </div>
   <div class="toast-container position-fixed top-0 end-0 p-3 " style="z-index: 1000; ">
     <div class="toast" id="liveToast" ref="toast">
       <div class="toast-header">
@@ -156,7 +159,8 @@ export default {
   props: ["name", "categories", "price", "synopsis","promotion","editor","author","tradutor","year","atClick","id","inBag","inLib"],
   data() {
     return {
-      categoriesCheck:[]
+      categoriesCheck:[],
+      updateCategories:0
     };
   },
   mounted() {
@@ -166,6 +170,15 @@ export default {
     }
   },
   methods: {
+    addCategory(newCategory){
+      let categories = this.getAllCategories();
+      let newCategoryObj = {};
+      newCategoryObj.name = newCategory;
+      newCategoryObj.id = categories[categories.length-1].id+1;
+      categories.push(newCategoryObj);
+      localStorage.setItem("categories",JSON.stringify(categories));
+      this.updateCategories++;
+    },
     getAllCategories() {
       return JSON.parse(localStorage.getItem("categories"))
     },
