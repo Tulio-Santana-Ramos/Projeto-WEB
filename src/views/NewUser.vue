8e2ac5cff78307a99d36b5731634e866e72ede7f
AnaClaraAmorim <anaclaramorimandrade@gmail.com>
2022-06-20 22:27:06 -0300

    comentarios de novo admin e de novo usuario

--- src/views/NewUser.vue
+++ src/views/NewUser.vue
@@ -4,10 +4,16 @@ import Menu from "@/components/Menu.vue";
 import Footer from "@/components/Footer.vue";
 </script>
 
+
+<!-- Página feita para adicionar um novo usuário no sistema -->
 <template>
 <Menu/>
   <h2 class="title">CRIAR CONTA</h2>
   
+  <!-- Campos necessários para cadastro de novo usuário com flag indicando que usuário
+  a ser cadastro não é admin (pois o mesmo componente é usado para cadastro de admin e 
+  usuário normal), e a props next page
+  passa qual é a próxima rota ao clicar no botão de criar conta -->
   <NewAccount
       :adminReg=false
       :nextPage="next"
