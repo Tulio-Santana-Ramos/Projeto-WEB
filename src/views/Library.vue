df169aecd07713da822c123ecb73e6c640d17602
EduardoPortoSilva <edu.porto51@gmail.com>
2022-06-20 23:40:02 -0300

    Add some comments and clean the code

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -1,8 +1,8 @@
 <script setup>
-import Books from "@/components/BigBooks.vue";
-import Footer from "@/components/Footer.vue";
-import Menu from "@/components/Menu.vue";
-import LoginRequired from "@/components/LoginRequired.vue";
+import Books from "../components/BigBooks.vue";
+import Footer from "../components/Footer.vue";
+import Menu from "../components/Menu.vue";
+import LoginRequired from "../components/LoginRequired.vue";
 import {VueCookieNext} from "vue-cookie-next";
 </script>
 
@@ -42,21 +42,45 @@ import {VueCookieNext} from "vue-cookie-next";
 export default {
   name: "app",
   methods: {
+    /**
+     * Adiciona as variáveis para a dropdown funcionar
+     * @param id
+     */
     filterDropdown(id) {
       this.$router.push({path: "/biblioteca", query: {category: id}});
     },
+    /**
+     * Faz a rota para visualizar as informações do livro
+     * @param idLivro
+     */
     goToBook(idLivro) {
       this.$router.push({path: "/livro", query: {id: idLivro}});
     },
+    /**
+     * Verifica se o usuário está logado
+     * @returns {boolean}
+     */
     hadUser() {
       return VueCookieNext.getCookie("account") !== null;
     },
+    /**
+     * Retorna todas as categorias
+     * @returns {any}
+     */
     getAllCategories() {
       return JSON.parse(localStorage.getItem("categories"))
     },
+    /**
+     * Retorna todos os livros
+     * @returns {any}
+     */
     getAllBooks() {
       return JSON.parse(localStorage.getItem("books"));
     },
+    /**
+     * Retorna todos os livros
+     * @returns {*[]|*}
+     */
     getActualBooks() {
       let temp = [];
       let books = this.getAllBooks();
@@ -64,23 +88,28 @@ export default {
         return books;
       let category = this.$route.query.category;
       for (const book of books) {
-        if (book.categories.includes(parseInt(category)))
+        if (book.categories.includes(parseInt(category[0])))
           temp.push(book);
       }
       return temp;
     },
+    /**
+     * Retorna todos os livros que o usuário tem na biblioteca
+     * @returns {*[]}
+     */
     getUserBooks() {
+      //Define algumas variáveis para a execução do código
       let actualUser = VueCookieNext.getCookie("account");
       let libraries = JSON.parse(localStorage.getItem("libraries"));
       let allCategories = this.getAllCategories();
       let temp = [];
-      for (const library of libraries) {
+      for (const library of libraries) {// Busca a biblioteca do usuário
         if (library.user === actualUser.id) {
-          for (const book of this.getActualBooks()) {
+          for (const book of this.getActualBooks()) {//Busca todos os livros do usuário
             for (const bookLib of library.lib) {
               if (parseInt(book.id) === parseInt(bookLib.id)) {
                 let tempCategories = [];
-                for (const category of book.categories) {
+                for (const category of book.categories) {//Transforma as categorias de número para texto
                   for (const fixedCategory of allCategories) {
                     if (category === fixedCategory.id) {
                       tempCategories.push(fixedCategory.name);
@@ -109,7 +138,7 @@ export default {
 </script>
 
 <style scoped>
-@import "@/assets/base.css";
+@import "../assets/base.css";
 
 .titlePage {
   color: #38b6ff;
@@ -118,31 +147,9 @@ export default {
 }
 
 .books {
-  padding: 0em 2vw;
+  padding: 0 2vw;
   display: flex;
   flex-wrap: wrap;
   min-height: 501px;
-}
-
-.NoLogin {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-  color: #38b6ff;
-  height: 51.7vh;
-}
-
-.btn.btn-primary {
-  margin-top: 50px;
-  width: 200px;
-  background-color: #38b6ff;
-  border-color: #38b6ff;
-}
-
-.btn.btn-primary:hover {
-  width: 200px;
-  background-color: #134968;
-  border-color: #134968;
 }
 </style>
