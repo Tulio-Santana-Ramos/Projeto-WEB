a3209394b79b16a2511b9e72e626d6a450b37cdc
AnaClaraAmorim <anaclaramorimandrade@gmail.com>
2022-06-20 23:17:04 -0300

    BagBook comentado

--- src/components/BagBook.vue
+++ src/components/BagBook.vue
@@ -1,15 +1,20 @@
+<!-- Mostra os livros suas categorias, capa, preço e se está em promoção-->
 <template>
   <div class="home-book">
     <img class="img-livro" v-bind:src="'/src/assets/' + id + '.jpg'"/>
     <div class="home-book-info">
-      <p class="title">{{ name }}</p>
+      <!-- Recebe por props o nome do livro -->
+      <p class="title">{{ name }}</p> 
+      <!-- Lista todas as categorias do livro -->
       <ul v-for="category in categories" class="category">
         <li>{{ category }}</li>
       </ul>
+      <!-- Verifica se o livro está em promoção ou não e exibe o preço do livro-->
       <p class="price" v-if="!promo()">R$ {{ price }}</p>
       <p class="price" v-else>R$ {{ promotion.tempPrice }}</p>
     </div>
     <div class="remove-button">
+      <!-- Permite remover o livro, a função que faz isso é passada por props-->
       <button type="button" class="btn btn-danger" @click="action(id)">Remover item</button>
     </div>
   </div>
